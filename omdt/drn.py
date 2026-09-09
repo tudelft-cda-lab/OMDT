2f8376c55eb29d7ab8747d07e94e9f7a9c3de0f8
Daniel Vos <daniel.vos.d@gmail.com>
2026-09-09 13:40:19 +0200

    Fix DRNs for older STORM versions

--- omdt/drn.py
+++ omdt/drn.py
@@ -7,10 +7,9 @@ def write_as_drn(file: Path, mdp: MarkovDecisionProcess):
     with open(file, "w") as f:
         f.write("// Exported by OMDT\n")
         f.write("@type: MDP\n")
-        f.write("@value_type: double\n")
         f.write(f"@nr_states\n{mdp.n_states_}\n")
         f.write(f"@nr_choices\n{mdp.n_states_ * mdp.n_actions_}\n")
-        f.write("@reward_model\nreward\n")
+        f.write("@reward_models\nreward\n")
         f.write("@model\n")
 
         for state in range(mdp.n_states_):
@@ -22,15 +21,18 @@ def write_as_drn(file: Path, mdp: MarkovDecisionProcess):
             observations = mdp.observations[state]
             obs = []
             for feature in range(len(observations)):
-                obs.append(f"{mdp.feature_names[feature]} = {observations[feature]}")
+                obs.append(f"{mdp.feature_names[feature]}={int(observations[feature])}")
             f.write(f"//[{' & '.join(obs)}]\n")
 
             for action in range(mdp.n_actions_):
                 rewards = mdp.rewards[state, :, action]
                 probs = mdp.trans_probs[state, :, action]
                 expected_reward = rewards.dot(probs)
-                f.write(f"\taction {mdp.action_names[action]} [{expected_reward}]\n")
+                action_name = mdp.action_names[action].replace(' ', '_').replace("'", "")
+                f.write(f"\taction {action_name} [{expected_reward}]\n")
                 for next_state in range(mdp.n_states_):
-                    f.write(
-                        f"\t\t{next_state} : {mdp.trans_probs[state, next_state, action]}\n"
-                    )
+                    probability = mdp.trans_probs[state, next_state, action]
+                    if probability > 1e-8:
+                        f.write(
+                            f"\t\t{next_state} : {probability}\n"
+                        )
